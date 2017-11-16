8988dbff9cac7d1be24e421561f7e74415a95524
AJ Taylor <ajtaylor@AJs-MacBook-Pro.local>
2017-11-15 16:31:20 -0800

    Added firacode and helm

--- init.el
+++ init.el
@@ -117,7 +117,119 @@ by Prelude.")
 (require 'prelude-clojure)
 (require 'clj-refactor)
 
-;; fira code setup
+;; helm setup
+
+(require 'helm-config)
+
+(setq helm-ff-transformer-show-only-basename nil
+      helm-adaptive-history-file "~/.emacs.d/data/helm-history"
+      helm-yank-symbol-first t
+      helm-move-to-line-cycle-in-source t
+      helm-buffers-fuzzy-matching t
+      helm-ff-auto-update-initial-value t)
+
+(autoload 'helm-descbinds "helm-descbinds" t)
+
+(autoload 'helm-eshell-history "helm-eshell" t)
+
+(autoload 'helm-esh-pcomplete "helm-eshell" t)
+
+(global-set-key (kbd "C-h a") 'helm-apropos)
+
+(global-set-key (kbd "C-h b") 'helm-descbinds)
+
+;; (add-hook 'eshell-mode-hook
+;; #'(lambda ()
+;; (define-key eshell-mode-map (kbd "TAB") #'helm-esh-pcomplete)
+;; (define-key eshell-mode-map (kbd "C-c C-l") #'helm-eshell-history)))
+
+(add-hook 'eshell-mode-hook
+(lambda ()
+(define-key eshell-mode-map (kbd "TAB") 'helm-esh-pcomplete)))
+
+(global-set-key (kbd "C-x b") 'helm-mini)
+
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
+
+(global-set-key (kbd "C-x r l") 'helm-filtered-bookmarks)
+
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+(global-set-key (kbd "M-s o") 'helm-swoop)
+
+(global-set-key (kbd "M-s /") 'helm-multi-swoop)
+
+(helm-mode t)
+
+;;(helm-adaptative-mode t)
+
+(global-set-key (kbd "C-x c!") 'helm-calcul-expression)
+
+(global-set-key (kbd "C-x c:") 'helm-eval-expression-with-eldoc)
+
+(define-key helm-map (kbd "M-o") 'helm-previous-source)
+
+(global-set-key (kbd "M-s s") 'helm-ag)
+
+;; (require 'helm-projectile)
+
+;; (setq helm-projectile-sources-list (cons 'helm-source-projectile-files-list
+;; (remove 'helm-source-projectile-files-list
+;; helm-projectile-sources-list)))
+;; (helm-projectile-on)
+
+;; (define-key projectile-mode-map (kbd "C-c p /")
+;; (lambda ()
+;; (interactive)
+;; (helm-ag (projectile-project-root))))
+
+;; (define-key org-mode-map (kbd "C-x c o h") #'helm-org-headlines)
+
+(require 'helm-config)
+
+;; (global-set-key (kbd "M-x") 'helm-M-x)
+
+(global-set-key (kbd "C-x b") 'helm-mini)
+
+;; Split frame horizontally and display helm results.
+(setq helm-split-window-default-side 'other
+helm-split-window-in-side-p t)
+
+(require 'helm-files)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(setq helm-ff-search-library-in-sexp t)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+
+(define-key helm-map (kbd "C-z") 'helm-select-action)
+
+(require 'helm-grep)
+
+(define-key helm-grep-mode-map (kbd "<return>") 'helm-grep-mode-jump-other-window)
+
+(define-key helm-grep-mode-map (kbd "n") 'helm-grep-mode-jump-other-window-forward)
+
+(define-key helm-grep-mode-map (kbd "p") 'helm-grep-mode-jump-other-window-backward)
+
+;; Caution: This solution might have unintended consequences, such as using grep on the buffers list to recurse on every listed file.
+(eval-after-load 'helm-grep
+'(setq helm-grep-default-command helm-grep-default-recurse-command))
+
+;; Add current position to mark ring.
+(add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
+
+;; TODO: Configure to work with helm-mode while company-mode is also available?
+(global-set-key (kbd "C-TAB") 'ac-complete-with-helm)
+
+;;;; fira code setup
 
 (when (window-system)
   (set-default-font "Fira Code"))
